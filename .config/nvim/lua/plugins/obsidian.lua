a6b3f0b4ed60006a9668c3f0aa5faf95b1a4be71
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2024-08-17 19:11:28 -0300

    Added obsidian daily notes and keymap

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -28,6 +28,19 @@ return {
         min_chars = 2,
       },
 
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "daily-notes",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = "%Y-%m-%d-%a",
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = "%B %-d, %Y",
+        -- Optional, default tags to add to each new daily note created.
+        default_tags = { "daily-notes" },
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = nil
+      },
+
       mappings = {
         -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
         ["gf"] = {

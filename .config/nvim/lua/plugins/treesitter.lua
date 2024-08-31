607cbfa0edf777db687f776395d96fa34940c1e9
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2024-08-31 19:10:22 -0300

    added inline markdown parser to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return{
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "python", "javascript", "html", "latex" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "python", "javascript", "html", "latex", "markdown_inline" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },

30edda7222020d498bcab30c321b7495f66b974c
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2024-09-22 20:36:08 -0300

    Changed obsidian.nvim to not include ID in the name of the file and to create links using the path to the file

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -98,9 +98,20 @@ return {
       ---@return string|obsidian.Path The full path to the new note.
       note_path_func = function(spec)
         -- This is equivalent to the default behavior.
-        local path = spec.dir / (tostring(spec.title) .. "__" .. tostring(spec.id))
+        local path = spec.dir / (tostring(spec.title))
         return path:with_suffix(".md")
       end,
+
+      -- Optional, customize how wiki links are formatted. You can set this to one of:
+      --  * "use_alias_only", e.g. '[[Foo Bar]]'
+      --  * "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
+      --  * "prepend_note_path", e.g. '[[foo-bar.md|Foo Bar]]'
+      --  * "use_path_only", e.g. '[[foo-bar.md]]'
+      -- Or you can set it to a function that takes a table of options and returns a string, like this:
+      -- wiki_link_func = function(opts)
+      --   return require("obsidian.util").wiki_link_id_prefix(opts)
+      -- end,
+      wiki_link_func = "prepend_note_path",
 
       -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
       -- URL it will be ignored but you can customize this behavior here.

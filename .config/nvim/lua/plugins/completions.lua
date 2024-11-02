ca2abd925ef843263d18ac8649d29b20e89bf331
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2024-11-02 09:15:12 -0300

    Fixed problem where a few markdown snippets where activated by Tab

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -47,8 +47,8 @@ return{
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ["<Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
+            if luasnip.jumpable() then
+              luasnip.jump()
             else
               fallback()
             end

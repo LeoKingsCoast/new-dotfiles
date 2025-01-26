5ebeaf801ff9fc03f22d0aaaa207c28aa564a6e7
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-01-26 09:07:10 -0300

    Disabled noice.nvim

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -29,11 +29,11 @@ return{
           'encoding',
           'fileformat',
           'filetype',
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          }
+          -- {
+          --   require("noice").api.statusline.mode.get,
+          --   cond = require("noice").api.statusline.mode.has,
+          --   color = { fg = "#ff9e64" },
+          -- }
         },
         lualine_y = {'progress'},
         lualine_z = {'location'}

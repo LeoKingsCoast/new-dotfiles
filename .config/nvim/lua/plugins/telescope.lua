d470af2bb3cb31210ea5af150268181efacced60
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-02-01 11:51:00 -0300

    Made telescope grep query a string before searching

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,9 @@ return {
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Find grep" })
+            vim.keymap.set('n', '<leader>fg', function ()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end, { desc = "Find grep" })
             vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers" })
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help tags" })
         end

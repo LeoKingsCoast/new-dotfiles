01344749ee54911ef98a6cec66417789e15d4d4a
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-04-22 20:18:14 -0300

    Set tab behavior

--- .config/nvim/lua/nvim_config.lua
+++ .config/nvim/lua/nvim_config.lua
@@ -1,3 +1,10 @@
+-- =============================== Behavior ==================================
+
+-- Set tab configuration
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 
 -- ================================ Yanking ==================================
 

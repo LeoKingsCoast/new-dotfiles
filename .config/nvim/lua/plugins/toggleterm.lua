5c9aac963d22f5a94d77a2b8eb253287a74bfff0
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-04-24 21:06:14 -0300

    Add toggleterm

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+
+    },
+    config = function ()
+        require("toggleterm").setup{
+            open_mapping = [[<c-]>]],
+            direction = 'float'
+        }
+    end
+}

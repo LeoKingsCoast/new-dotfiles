47399ef14e37978a7406f6041c8650ea3e580c06
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-04-27 14:52:57 -0300

    Configure telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,16 @@
 return {
-
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
+            vim.keymap.set('n', '<leader>fg', function ()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end, { desc = "Find grep" })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers" })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help tags" })
+        end
+    },
 }

277c6a5c41d1676c5967587bbb2a3afea1d203d5
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2025-07-06 20:10:19 -0300

    Add better html lsp support

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -16,16 +16,30 @@ return {
           "rust_analyzer",
           "clangd",
           "marksman",
+          "ts_ls",
           "html",
+          "emmet_ls",
           "pyright",
           "autotools_ls",
           "arduino_language_server"
         },
         handlers = {
           function (server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup ({
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            local server = require("lspconfig")[server_name].setup ({
               capabilities = capabilities,
               inlay_hints = { enabled = true },
+            })
+
+            local lspconfig = require('lspconfig')
+
+            lspconfig.html.setup({
+              settings = {
+                html = {
+                  autoClosingTags = true, -- this is optional, the behavior comes from snippets too
+                }
+              },
+              capabilities = require('cmp_nvim_lsp').default_capabilities(),
             })
           end,
         }

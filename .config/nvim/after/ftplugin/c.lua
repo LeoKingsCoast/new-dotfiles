cf036bdfd90e13a72239287874c763b28bbd44be
Leo <114858492+LeoKingsCoast@users.noreply.github.com>
2026-02-03 07:08:00 -0300

    Configure C formatting

---
+++ .config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,6 @@
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = true
+vim.opt_local.indentexpr = ""
+vim.opt_local.cindent = true
+vim.opt_local.cinoptions = ':0,l1,g0,(0,W4,m1'
